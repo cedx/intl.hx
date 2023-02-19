1d1caeae60ab164094008114564bbb7b9e199710
Cédric Belin <cedric@belin.io>
2023-02-20 00:30:31 +0100

    Added support for time zones to the `DateFormat` component

--- src/intl/DateFormat.hx
+++ src/intl/DateFormat.hx
@@ -1,51 +1,45 @@
 package intl;
 
 #if java
-import java.text.DateFormat as NativeDateFormat;
-import java.util.Date as NativeDate;
-import java.util.Locale;
+import java.text.DateFormat as JavaDateFormat;
+import java.util.Date as JavaDate;
+import java.util.TimeZone;
 #elseif js
-import js.lib.Date as NativeDate;
-import js.lib.intl.DateTimeFormat as NativeDateFormat;
-#elseif php
-import php.DateTime as NativeDate;
-import php.IntlDateFormatter as NativeDateFormat;
+import js.lib.Date as JsDate;
+import js.lib.intl.DateTimeFormat;
 #else
-#error "This compilation target is not supported."
+import php.DateTime;
+import php.IntlDateFormatter;
 #end
 
 /** Formats dates in a locale-dependent manner. **/
-abstract DateFormat(NativeDateFormat) to NativeDateFormat {
+abstract DateFormat(NativeDateFormat) from NativeDateFormat to NativeDateFormat {
 
 	/** Creates a new date format. **/
 	public function new(locale: String, options: DateFormatOptions) {
 		#if java
-			final locale = Locale.forLanguageTag(locale);
-			switch options {
-				case {dateStyle: dateStyle, timeStyle: null} if (dateStyle != null): this = NativeDateFormat.getDateInstance(dateStyle, locale);
-				case {dateStyle: null, timeStyle: timeStyle} if (timeStyle != null): this = NativeDateFormat.getTimeInstance(timeStyle, locale);
-				default: this = NativeDateFormat.getDateTimeInstance(options.dateStyle, options.timeStyle, locale);
+			final locale = new Locale(locale);
+			this = switch options {
+				case {dateStyle: dateStyle, timeStyle: null} if (dateStyle != null): JavaDateFormat.getDateInstance(dateStyle, locale);
+				case {dateStyle: null, timeStyle: timeStyle} if (timeStyle != null): JavaDateFormat.getTimeInstance(timeStyle, locale);
+				default: JavaDateFormat.getDateTimeInstance(options.dateStyle, options.timeStyle, locale);
 			}
+
+			if (options.timeZone != null) this.setTimeZone(TimeZone.getTimeZone(options.timeZone));
 		#elseif js
-			this = new NativeDateFormat(locale, cast options);
+			this = new DateTimeFormat(locale, cast options);
 		#else
-			final dateStyle = options.dateStyle != null ? options.dateStyle : NativeDateFormat.NONE;
-			final timeStyle = options.timeStyle != null ? options.timeStyle : NativeDateFormat.NONE;
-			this = new NativeDateFormat(locale, dateStyle, timeStyle);
+			final dateStyle = options.dateStyle != null ? options.dateStyle : IntlDateFormatter.NONE;
+			final timeStyle = options.timeStyle != null ? options.timeStyle : IntlDateFormatter.NONE;
+			this = new IntlDateFormatter(locale, dateStyle, timeStyle, options.timeZone);
 		#end
 	}
 
 	/** Formats the specified `date`. **/
-	public function format(date: Date): String {
-		#if java
-			return this.format(new NativeDate(date.getTime()));
-		#elseif js
-			return this.format(NativeDate.fromHaxeDate(date));
-		#else
-			final timestamp = date.getTime() / 1000;
-			return this.format(new NativeDate('@$timestamp'));
-		#end
-	}
+	public function format(date: Date) return
+		#if java this.format(new JavaDate(date.getTime()))
+		#elseif js this.format(JsDate.fromHaxeDate(date))
+		#else this.format(new DateTime('@${Std.int(date.getTime() / 1000)}')) #end;
 }
 
 /** Defines the options of a `DateFormat` instance. **/
@@ -56,6 +50,9 @@ typedef DateFormatOptions = {
 
 	/** The time style. **/
 	var ?timeStyle: DateTimeStyle;
+
+	/** The time zone. **/
+	var ?timeZone: String;
 }
 
 /** Provides static extensions for dates. **/
@@ -63,7 +60,7 @@ abstract class DateFormatTools {
 
 	/** Converts the specified `date` to a locale-dependent string. **/
 	public static inline function toLocaleString(date: Date, locale: String, options: DateFormatOptions) return
-		#if js NativeDate.fromHaxeDate(date).toLocaleString(locale, cast options)
+		#if js JsDate.fromHaxeDate(date).toLocaleString(locale, cast options)
 		#else new DateFormat(locale, options).format(date) #end;
 }
 
@@ -82,3 +79,6 @@ enum abstract DateTimeStyle(#if js String #else Int #end) to #if js String #else
 	/** Short style. **/
 	var Short = #if js "short" #else 3 #end;
 }
+
+/** The underlying native date format. **/
+private typedef NativeDateFormat = #if java JavaDateFormat #elseif js DateTimeFormat #else IntlDateFormatter #end;
